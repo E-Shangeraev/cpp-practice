6af7642f212f5b647a12517827de9f5b3f45bbdd
E-Shangeraev <e.shangeraev@gmail.com>
2024-05-01 16:27:44 +0600

    fix: a few bugs with printing

--- main.cpp
+++ main.cpp
@@ -3,18 +3,17 @@
 
 using namespace std;
 
-template <size_t N>
-void printArray(int (&array)[N]) {
+void printArray(int array[], int size) {
   cout << endl;
 
-  for (int &i : array) {
+  for (int i = 0; i < size; i++) {
     cout << array[i] << ' ';
   }
 }
 
-void bubbleSort(int array[], const int size) {
+void bubbleSort(int array[], int size) {
   int arrayCopy[size];
-  memcpy(arrayCopy, array, size); 
+  memcpy(arrayCopy, array, size * sizeof(array[0]));
 
   for (int i = 0; i < size - 1; i++) {
     for (int j = 0; j < size - i - 1; j++) {
@@ -26,11 +25,11 @@ void bubbleSort(int array[], const int size) {
     }
   }
 
-  printArray(arrayCopy);
+  printArray(arrayCopy, size);
 }
 
 int main() {
-  int array[] = {5, 3, 7, 8, 2, 6, 1, 9, 4, 0};
+  int array[] {5, 3, 7, 8, 2, 6, 1, 9, 4, 0};
   const int size = sizeof(array) / sizeof(array[0]);
 
   bubbleSort(array, size);
